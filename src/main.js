5a22ceb6fd8101d984d7638d477715081d2133aa
Gabino Santana <gabino.santanacabrera@mercadolibre.com>
2019-12-17 23:38:47 -0300

    Kick off proyecto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+import Chart from 'chart.js'
 
-Vue.config.productionTip = false;
+Vue.use(Chartkick.use(Chart))
+Vue.use(VueSpinners)
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
